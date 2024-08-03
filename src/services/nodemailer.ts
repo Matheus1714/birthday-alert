869ca0fa4a2a8d4ae9694e9e7ab5699116d95c72
matheus1714 <matheussilvamartins1714@gmail.com>
2024-08-03 18:23:27 -0300

    feat: create service to send email using nodemailer

---
+++ src/services/nodemailer.ts
@@ -0,0 +1,23 @@
+import { createTransport } from "nodemailer";
+require("dotenv").config();
+
+/**
+ * Create a application with a google password
+ * @link{https://myaccount.google.com/apppasswords}
+ *
+ * Information about it
+ * @link{https://support.google.com/mail/thread/205453566/how-to-generate-an-app-password?hl=en}
+ */
+
+const transporter = createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.ADMIN_EMAIL,
+    pass: process.env.ADMIN_PASSWORD,
+  },
+});
+
+export { transporter };
