d14fd326d17f249ba9614c2582c3094340a54931
matheus1714 <matheussilvamartins1714@gmail.com>
2024-08-03 22:16:46 -0300

    feat: add more enviroments variables

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -3,6 +3,21 @@ declare global {
     interface ProcessEnv {
       ADMIN_EMAIL: string;
       ADMIN_PASSWORD: string;
+
+      GOOGLE_SHEET_ID: string;
+      GOOGLE_SHEET_RANGE: string;
+
+      GCP_TYPE: string;
+      GCP_PROJECT_ID: string;
+      GCP_PRIVATE_KEY_ID: string;
+      GCP_PRIVATE_KEY: string;
+      GCP_CLIENT_EMAIL: string;
+      GCP_CLIENT_ID: string;
+      GCP_AUTH_URI: string;
+      GCP_TOKEN_URI: string;
+      GCP_AUTH_PROVIDER_X509_CERT_URL: string;
+      GCP_CLIENT_X509_CERT_URL: string;
+      GCP_UNIVERSE_DOMAIN: string;
     }
   }
 }
