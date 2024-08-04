f0b7ebce4d200820e5717d7bd6c51fc7c81f5dbe
matheus1714 <matheussilvamartins1714@gmail.com>
2024-08-03 22:18:27 -0300

    feat: create logic to get sheet data

---
+++ src/get-data-from-google-sheets.ts
@@ -0,0 +1,36 @@
+import { auth, googleSheets } from "./services/google-sheets";
+
+export async function getDataFromGoogleSheets(): Promise<
+  { name: string; birthday: Birthday }[]
+> {
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range: process.env.GOOGLE_SHEET_RANGE,
+  });
+
+  const values = getRows.data.values;
+
+  if (!values || !values?.length || !values[0]?.length) return [];
+
+  const people: { name: string; birthday: Birthday }[] = [];
+
+  values.forEach((row) => {
+    const name = String(row.at(0));
+    const day = Number(row.at(1));
+    const month = Number(row.at(2)) as Month | undefined;
+    const year = Number(row.at(3));
+
+    const isNumeric = !day || !month || !year || month < 1;
+    const inValidMonths = month ? month > 0 || month < 13 : false;
+
+    if (!isNumeric || !inValidMonths) return;
+
+    people.push({
+      name,
+      birthday: { day, month: month as Month, year },
+    });
+  });
+
+  return people;
+}
